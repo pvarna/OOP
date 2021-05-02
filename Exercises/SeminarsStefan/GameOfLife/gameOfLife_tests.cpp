853dac815d215c7bb7406fdc60c739ec6c4685c3
pvarna <peter.kolev2001@gmail.com>
2021-05-03 01:58:06 +0300

    add "Game of life"

--- Exercises/SeminarsStefan/GameOfLife/gameOfLife_tests.cpp
+++ Exercises/SeminarsStefan/GameOfLife/gameOfLife_tests.cpp
@@ -3,6 +3,7 @@
 #include "gameOfLife.h" 
 
 /*
+--Main rules--
 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
 Any live cell with two or three live neighbours lives on to the next generation.
 Any live cell with more than three live neighbours dies, as if by overpopulation.
